1b178e157893bfd898eb59d14c72a49bbca372c9
NTA <nta@NTAs-MacBook-Pro.local>
2021-04-17 23:55:18 +0700

    17/4

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+// function swift
+func addvalue()
+{
+    let a = 10
+    let b = 20
+    
+    let c = a + b
+    print(c)
+}
+
+func nhan()
+{
+    let a = 0
+    let b = 100
+    let c = a * b
+    print(c)
+}
+
+// chay funtion
+nhan()
+
+func greeting(person: String) -> String
+{
+    let greeting = "Heloo, " + person + "!"
+    return greeting
+}
+print(greeting(person:"Ngo Truong Anh"))
+
+// order
+func sayhello() -> String
+{
+    return "Hello World"
+}
+print(sayhello())
+
+//order
+
